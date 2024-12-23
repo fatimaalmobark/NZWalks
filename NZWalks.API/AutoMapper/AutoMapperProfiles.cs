06fb9374f920d40fbbfa77dd1a4c6ba2b31a172f
fatimaMobark <135317309+fatimaalmobark@users.noreply.github.com>
2024-12-23 10:28:43 +0200

    ##

---
+++ NZWalks.API/AutoMapper/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.AutoMapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+          CreateMap<Region,RegionDTO>().ReverseMap();
+        }
+    }
+
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using NZWalks.API.AutoMapper;
 using NZWalks.API.Data;
 using NZWalks.API.Repositries;
 
@@ -13,6 +14,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 builder.Services.AddScoped<IRegionRepositry,SqlRegionRepositry>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
