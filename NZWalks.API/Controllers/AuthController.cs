72922842b8148bc2fdedad1c1cd9f5784760a6d8
fatimaMobark <135317309+fatimaalmobark@users.noreply.github.com>
2025-01-05 15:05:31 +0200

    ##

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
